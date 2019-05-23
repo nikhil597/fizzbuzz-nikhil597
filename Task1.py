e7ac7f9fba03547165d778b39f715856b16b2911
puneetverma100 <Puneet.Verma@dal.ca>
2019-05-23 07:04:51 -0700

    text file copying with 133t code

---
+++ Task1.py
@@ -0,0 +1,28 @@
+
+path = '/home/student/fizzbuzz-nikhil597/targetfile.txt'
+import string
+
+f = open("/home/student/fizzbuzz-nikhil597/file.txt","r")
+targetfile = open("/home/student/fizzbuzz-nikhil597/targetfile.txt","w+")
+for i in range(text):
+	for i,value in enumerate(targetfile):
+		n=value.slipt("/n")
+		for letter in n:
+		print(letter)
+		if letter == "o"||"O":
+			letter = 0
+			fout.write(letter)
+		elif letter == "a"||"A":
+			letter = 4
+			fout.write(letter)
+		elif letter == "e"||"E":
+			letter = 3
+			fout.write(letter)
+		elif letter == "i"||"I":
+			letter = 1
+			fout.write(letter)
+		elif letter == "er"
+			letter = xor
+			fout.write(letter)
+	targetfile.write(text)
+	print(targetfile.readline)
