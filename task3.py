200818f16cd964eac50c90a7c8cb884bb3f6224a
puneetverma100 <Puneet.Verma@dal.ca>
2019-05-23 07:54:01 -0700

    word play do not present in text files

---
+++ task3.py
@@ -0,0 +1,18 @@
+def wordsearch(file1,file2)
+        try:
+                f1=open("/home/student/openbookfinal-2019-nikhil597/20k.txt","r$
+                f2=open("/home/student/openbookfinal-2019-nikhil597/Book1.txt",$
+                f1read=f1.readline()
+                f1.close()
+                for i in f2:
+                        j = i.lower()
+                        target = 0
+                        for k in read:
+                                f1=k.split()
+                                for l in f2
+                                        a = l.lower()
+                                if l==1:
+                                        target +=1
+                                        print(l,target)
+
+
